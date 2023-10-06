7bc83acda55edb1a71b500cb19664a667d6f381d
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-10-06 10:23:16 -0400

    apagado linhas & reprogramado tarefas

--- gruntFile.js
+++ gruntFile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
     less: {
       development: {
         files: {
-          "main.css": "main.less", // Define a saída do CSS.
+          "dev/styles/main.css": "src/styles/main.less", // Define a saída do CSS.
         },
       },
       // Configuração da tarefa 'less' para compilar arquivos LESS em CSS (production).
@@ -23,61 +23,17 @@ module.exports = function (grunt) {
           compress: true, // Habilita a compressão do CSS.
         },
         files: {
-          "main.min.css": "main.less", // Define o nome do arquivo de saída minificado.
+          "dist/styles/main.min.css": "src/styles/main.less", // Define o nome do arquivo de saída minificado.
         },
       },
     },
-    sass: {
-      dist: {
-        // A tarefa 'sass' é configurada para compilar arquivos SCSS em CSS.
-        // Define opções específicas para a compilação.
-        options: {
-          style: "compressed", // Habilita a compressão do CSS.
-        },
-        // Define os arquivos de entrada (SCSS) e os arquivos de saída (CSS).
-        files: {
-          "main2.css": "main.scss", // Define o nome do arquivo de saída minificado.
-        },
-      },
-    },
-    concurrent: {
-      target: ["olaGrunt", "less", "sass", "tarefaDemorada"],
-    },
-  });
-
-  // Registra uma tarefa personalizada chamada 'olaGrunt'.
-  grunt.registerTask("olaGrunt", function () {
-    // Captura a função 'done' para indicar o término da tarefa.
-    const done = this.async();
-    // Após 3 segundos, imprime "Olá Grunt" no console.
-    setTimeout(function () {
-      console.log("Olá Grunt");
-      // Indica que a tarefa foi concluída.
-      done();
-    }, 3000);
-  });
-
-  // Registra uma tarefa personalizada chamada 'olaGrunt'.
-  grunt.registerTask("tarefaDemorada", function () {
-    // Captura a função 'done' para indicar o término da tarefa.
-    const done = this.async();
-    // Após 3 segundos, imprime "Olá Grunt" no console.
-    setTimeout(function () {
-      console.log("Olá Grunt");
-      // Indica que a tarefa foi concluída.
-      done();
-    }, 3000);
   });
 
   // Carrega o plugin 'grunt-contrib-less' para a tarefa 'less'.
   grunt.loadNpmTasks("grunt-contrib-less");
 
-  // Carrega o plugin 'grunt-contrib-sass' para a tarefa 'sass'.
-  grunt.loadNpmTasks("grunt-contrib-sass");
-
-  // Carrega o plugin 'grunt-concurrent' para acessar tarefas de forma paralela
-  grunt.loadNpmTasks("grunt-concurrent");
-
   // Registra a tarefa padrão 'default', que depende da tarefa 'less'.
-  grunt.registerTask("default", ["concurrent"]);
+  grunt.registerTask("default", ["less:development"]);
+
+  grunt.registerTask("build", ["less:production"]);
 };
